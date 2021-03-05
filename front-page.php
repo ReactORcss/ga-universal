9455be076b3e2cc0cdddab0149a7f4e4f3584e8b
Sorokin Alexey <alexwsop@gmail.com>
2021-03-05 15:05:15 +0300

    DZ11

--- front-page.php
+++ front-page.php
@@ -8,7 +8,8 @@
                 global $post;
 
                 $myposts = get_posts([ 
-                    'numberposts' => 1
+                    'numberposts' => 1,
+                    'category_name' => 'javascript, css, html, web-design',
                 ]);
                 //проверяем, есть ли посты
                 if( $myposts ){
@@ -16,25 +17,29 @@
                     foreach( $myposts as $post ){
                         setup_postdata( $post );
                         ?>
-                        <!-- Выводим записи -->
-                            <img src="<?php the_post_thumbnail_url()?>" alt="" class="post-thumb">
-                            <?php $author_id = get_the_author_meta('ID')?>
-                            <a href="<?php echo get_author_posts_url($author_id)?>" class="author">
+                    <!-- Выводим записи -->
+                    <img src="<?php the_post_thumbnail_url()?>" alt="" class="post-thumb">
+                    <?php $author_id = get_the_author_meta('ID')?>
+                    <a href="<?php echo get_author_posts_url($author_id)?>" class="author">
                                 <img src="<?php echo get_avatar_url($author_id)?>" alt="" class="avatar">
                                 <div class="author-bio">
                                     <span class="author-name"><?php the_author()?></span>
                                     <span class="author-rank">Должность</span>
                                 </div>
                             </a>
-                            <div class="post-text">
-                                <?php the_category() ?>
-                                <h2 class="post-title"><?php the_title() ?></h2>
-                                <a href="<?php echo get_the_permalink() ?>" class="more">Читать далее</a>
-                            </div>
+                    <div class="post-text">
+                        <?php the_category() ?>
+                        <h2 class="post-title">
+                            <?php echo mb_strimwidth(get_the_title(), 0, 60, '...') ?>
+                        </h2>
+                        <a href="<?php echo get_the_permalink() ?>" class="more">Читать далее</a>
+                    </div>
                     <?php 
                     }
                 } else {
-                    ?> <p>Постов нет</p> <?php
+                    ?>
+                    <p>Постов нет</p>
+                    <?php
                 }
                 wp_reset_postdata(); // Сбрасываем $post
                 ?>
@@ -48,7 +53,8 @@
 
                     $myposts = get_posts([ 
                         'numberposts' => 5,
-                        'offset' => 1
+                        'offset' => 1,
+                        'category_name' => 'javascript, css, html, web-design'
                     ]);
                     //проверяем, есть ли посты
                     if( $myposts ){
@@ -56,22 +62,59 @@
                         foreach( $myposts as $post ){
                             setup_postdata( $post );
                             ?>
-                            <!-- Выводим записи -->
-                            <li class="post">
+                        <!-- Выводим записи -->
+                        <li class="post">
                             <?php the_category() ?>
-                                <a class="post-permalink" href="<?php echo get_the_permalink()?>">
-                                <h4 class="post-title"><?php the_title() ?></h4>
-                                </a>
-                            </li>
-                            <?php 
+                            <a class="post-permalink" href="<?php echo get_the_permalink()?>">
+                                <h4 class="post-title">
+                                    <?php echo mb_strimwidth(get_the_title(), 0, 60, '...') ?>
+                                </h4>
+                            </a>
+                        </li>
+                        <?php 
                             }
                                 } else {
-                                    ?> <p>Постов нет</p> <?php
+                                    ?>
+                        <p>Постов нет</p>
+                        <?php
                                 }
                             wp_reset_postdata(); // Сбрасываем $post
                             ?>
                 </ul>
             </div>
-        </div>     
+        </div>
     </div>
 </main>
+
+<div class="container">
+    <ul class="article-list">
+        <?php
+            //объявляем глобальную переменную 
+                global $post;
+
+                $myposts = get_posts([ 
+                'numberposts' => 4,
+                'category_name' => 'articles'
+                ]);
+                //проверяем, есть ли посты
+                if( $myposts ){
+                //если есть, то запускаем цикл
+                foreach( $myposts as $post ){
+                setup_postdata( $post );
+                ?>
+        <!-- Выводим записи -->
+        <li class="article-item">
+            <a class="article-permalink" href="<?php echo get_the_permalink()?>">
+                    <h4 class="article-title"><?php echo mb_strimwidth(get_the_title(), 0, 50, '...')?></h4> <!-- обрезаем article до 50 символов-->
+            </a>
+            <img width="65" height="65" src="<?php echo get_the_post_thumbnail_url( null, 'homepage-thumb' )?>" alt="">
+        </li>
+            <?php 
+                }
+            } else {
+                ?><p>Постов нет</p><?php
+            }
+        wp_reset_postdata(); // Сбрасываем $post
+        ?>
+    </ul>
+</div>

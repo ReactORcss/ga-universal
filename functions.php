5cf71e664e3b37f1f7107891a39660ddffdab523
Sorokin Alexey <alexwsop@gmail.com>
2021-04-04 16:54:58 +0300

    DZ36addedTranslate

--- functions.php
+++ functions.php
@@ -2,6 +2,8 @@
 //Добавление расширенных возможностей
 if ( ! function_exists( 'universal_theme_setup' ) ) :
     function universal_theme_setup() {
+		//Подключение файлов перевода
+		load_theme_textdomain( 'universal', get_template_directory() . '/languages' );
         // Добавление тега title
         add_theme_support( 'title-tag' );
 
@@ -27,16 +29,16 @@ if ( ! function_exists( 'universal_theme_setup' ) ) :
 			register_post_type( 'lesson', [
 				'label'  => null,
 				'labels' => [
-					'name'               => 'Уроки', // основное название для типа записи
-					'singular_name'      => 'Урок', // название для одной записи этого типа
-					'add_new'            => 'Добавить урок', // для добавления новой записи
-					'add_new_item'       => 'Добавление урока', // заголовка у вновь создаваемой записи в админ-панели.
-					'edit_item'          => 'Редактирование урока', // для редактирования типа записи
-					'new_item'           => 'Новый урок', // текст новой записи
-					'view_item'          => 'Смотреть урок', // для просмотра записи этого типа.
-					'search_items'       => 'Искать урок', // для поиска по этим типам записи
-					'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
-					'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
+					'name'               => _x( 'Lesson', 'taxonomy general name', 'universal' ), // основное название для типа записи
+					'singular_name'      => _x( 'Lesson', 'taxonomy singular name', 'universal' ), // название для одной записи этого типа
+					'add_new'            => __( 'Add lesson', 'universal' ), // для добавления новой записи
+					'add_new_item'       => __( 'Add lesson', 'universal' ), // заголовка у вновь создаваемой записи в админ-панели.
+					'edit_item'          => __( 'Edit lesson', 'universal' ), // для редактирования типа записи
+					'new_item'           => __( 'New lesson', 'universal' ), // текст новой записи
+					'view_item'          => __( 'Watch lesson', 'universal' ), // для просмотра записи этого типа.
+					'search_items'       => __( 'Search lesson', 'universal' ), // для поиска по этим типам записи
+					'not_found'          => __( 'Not found', 'universal' ), // если в результате поиска ничего не было найдено
+					'not_found_in_trash' => __( 'Not found in the basket', 'universal' ), // если не было найдено в корзине
 					'parent_item_colon'  => '', // для родителей (у древовидных типов)
 					'menu_name'          => 'Уроки', // название меню
 				],
@@ -75,17 +77,17 @@ if ( ! function_exists( 'universal_theme_setup' ) ) :
 			register_taxonomy('genre', array('lesson'), array(
 				'hierarchical'  => true,
 				'labels'        => array(
-					'name'              => _x( 'Genres', 'taxonomy general name' ),
-					'singular_name'     => _x( 'Genre', 'taxonomy singular name' ),
-					'search_items'      =>  __( 'Search Genres' ),
-					'all_items'         => __( 'All Genres' ),
-					'parent_item'       => __( 'Parent Genre' ),
-					'parent_item_colon' => __( 'Parent Genre:' ),
-					'edit_item'         => __( 'Edit Genre' ),
-					'update_item'       => __( 'Update Genre' ),
-					'add_new_item'      => __( 'Add New Genre' ),
-					'new_item_name'     => __( 'New Genre Name' ),
-					'menu_name'         => __( 'Genre' ),
+					'name'              => _x( 'Genres', 'taxonomy general name', 'universal' ),
+					'singular_name'     => _x( 'Genre', 'taxonomy singular name', 'universal' ),
+					'search_items'      =>  __( 'Search Genres', 'universal' ),
+					'all_items'         => __( 'All Genres', 'universal' ),
+					'parent_item'       => __( 'Parent Genre', 'universal' ),
+					'parent_item_colon' => __( 'Parent Genre:', 'universal' ),
+					'edit_item'         => __( 'Edit Genre', 'universal' ),
+					'update_item'       => __( 'Update Genre', 'universal' ),
+					'add_new_item'      => __( 'Add New Genre', 'universal' ),
+					'new_item_name'     => __( 'New Genre Name', 'universal' ),
+					'menu_name'         => __( 'Genre', 'universal' ),
 				),
 				'show_ui'       => true,
 				'query_var'     => true,
@@ -96,21 +98,21 @@ if ( ! function_exists( 'universal_theme_setup' ) ) :
 			register_taxonomy('writer', 'lesson',array(
 				'hierarchical'  => false,
 				'labels'        => array(
-					'name'                        => _x( 'Teachers', 'taxonomy general name' ),
-					'singular_name'               => _x( 'Teacher', 'taxonomy singular name' ),
-					'search_items'                =>  __( 'Search Teachers' ),
-					'popular_items'               => __( 'Popular Teachers' ),
-					'all_items'                   => __( 'All Teachers' ),
+					'name'                        => _x( 'Teachers', 'taxonomy general name', 'universal' ),
+					'singular_name'               => _x( 'Teacher', 'taxonomy singular name', 'universal' ),
+					'search_items'                =>  __( 'Search Teachers', 'universal' ),
+					'popular_items'               => __( 'Popular Teachers', 'universal' ),
+					'all_items'                   => __( 'All Teachers', 'universal' ),
 					'parent_item'                 => null,
 					'parent_item_colon'           => null,
-					'edit_item'                   => __( 'Edit Teacher' ),
-					'update_item'                 => __( 'Update Teacher' ),
-					'add_new_item'                => __( 'Add New Teacher' ),
-					'new_item_name'               => __( 'New Teacher Name' ),
-					'separate_items_with_commas'  => __( 'Separate Teachers with commas' ),
-					'add_or_remove_items'         => __( 'Add or remove teachers' ),
-					'choose_from_most_used'       => __( 'Choose from the most used writers' ),
-					'menu_name'                   => __( 'Teachers' ),
+					'edit_item'                   => __( 'Edit Teacher', 'universal' ),
+					'update_item'                 => __( 'Update Teacher', 'universal' ),
+					'add_new_item'                => __( 'Add New Teacher', 'universal' ),
+					'new_item_name'               => __( 'New Teacher Name', 'universal' ),
+					'separate_items_with_commas'  => __( 'Separate Teachers with commas', 'universal' ),
+					'add_or_remove_items'         => __( 'Add or remove teachers', 'universal' ),
+					'choose_from_most_used'       => __( 'Choose from the most used writers', 'universal' ),
+					'menu_name'                   => __( 'Преподаватели', 'universal' ),
 				),
 				'show_ui'       => true,
 				'query_var'     => true,
@@ -663,3 +665,221 @@ function plural_form($number, $after) {
 	$cases = array (2, 0, 1, 1, 1, 2);
 	echo $number.' '.$after[ ($number%100>4 && $number%100<20)? 2: $cases[min($number%10, 5)] ];
 }
+
+/*
+ * "Хлебные крошки" для WordPress
+ * автор: Dimox
+ * версия: 2019.03.03
+ * лицензия: MIT
+*/
+function the_breadcrumbs() {
+
+	/* === ОПЦИИ === */
+	$text['home']     = 'Главная'; // текст ссылки "Главная"
+	$text['category'] = '%s'; // текст для страницы рубрики
+	$text['search']   = 'Результаты поиска по запросу "%s"'; // текст для страницы с результатами поиска
+	$text['tag']      = 'Записи с тегом "%s"'; // текст для страницы тега
+	$text['author']   = 'Статьи автора %s'; // текст для страницы автора
+	$text['404']      = 'Ошибка 404'; // текст для страницы 404
+	$text['page']     = 'Страница %s'; // текст 'Страница N'
+	$text['cpage']    = 'Страница комментариев %s'; // текст 'Страница комментариев N'
+
+	$wrap_before    = '<div class="breadcrumbs" itemscope itemtype="http://schema.org/BreadcrumbList">'; // открывающий тег обертки
+	$wrap_after     = '</div><!-- .breadcrumbs -->'; // закрывающий тег обертки
+	$sep            = '<span class="breadcrumbs__separator"> › </span>'; // разделитель между "крошками"
+	$before         = '<span class="breadcrumbs__current">'; // тег перед текущей "крошкой"
+	$after          = '</span>'; // тег после текущей "крошки"
+
+	$show_on_home   = 0; // 1 - показывать "хлебные крошки" на главной странице, 0 - не показывать
+	$show_home_link = 1; // 1 - показывать ссылку "Главная", 0 - не показывать
+	$show_current   = 1; // 1 - показывать название текущей страницы, 0 - не показывать
+	$show_last_sep  = 1; // 1 - показывать последний разделитель, когда название текущей страницы не отображается, 0 - не показывать
+	/* === КОНЕЦ ОПЦИЙ === */
+
+	global $post;
+	$home_url       = home_url('/');
+	$link           = '<span itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">';
+	$link          .= '<a class="breadcrumbs__link" href="%1$s" itemprop="item"><span itemprop="name">%2$s</span></a>';
+	$link          .= '<meta itemprop="position" content="%3$s" />';
+	$link          .= '</span>';
+	$parent_id      = ( $post ) ? $post->post_parent : '';
+	$home_link      = sprintf( $link, $home_url, $text['home'], 1 );
+
+	if ( is_home() || is_front_page() ) {
+
+		if ( $show_on_home ) echo $wrap_before . $home_link . $wrap_after;
+
+	} else {
+
+		$position = 0;
+
+		echo $wrap_before;
+
+		if ( $show_home_link ) {
+			$position += 1;
+			echo $home_link;
+		}
+
+		if ( is_category() ) {
+			$parents = get_ancestors( get_query_var('cat'), 'category' );
+			foreach ( array_reverse( $parents ) as $cat ) {
+				$position += 1;
+				if ( $position > 1 ) echo $sep;
+				echo sprintf( $link, get_category_link( $cat ), get_cat_name( $cat ), $position );
+			}
+			if ( get_query_var( 'paged' ) ) {
+				$position += 1;
+				$cat = get_query_var('cat');
+				echo $sep . sprintf( $link, get_category_link( $cat ), get_cat_name( $cat ), $position );
+				echo $sep . $before . sprintf( $text['page'], get_query_var( 'paged' ) ) . $after;
+			} else {
+				if ( $show_current ) {
+					if ( $position >= 1 ) echo $sep;
+					echo $before . sprintf( $text['category'], single_cat_title( '', false ) ) . $after;
+				} elseif ( $show_last_sep ) echo $sep;
+			}
+
+		} elseif ( is_search() ) {
+			if ( get_query_var( 'paged' ) ) {
+				$position += 1;
+				if ( $show_home_link ) echo $sep;
+				echo sprintf( $link, $home_url . '?s=' . get_search_query(), sprintf( $text['search'], get_search_query() ), $position );
+				echo $sep . $before . sprintf( $text['page'], get_query_var( 'paged' ) ) . $after;
+			} else {
+				if ( $show_current ) {
+					if ( $position >= 1 ) echo $sep;
+					echo $before . sprintf( $text['search'], get_search_query() ) . $after;
+				} elseif ( $show_last_sep ) echo $sep;
+			}
+
+		} elseif ( is_year() ) {
+			if ( $show_home_link && $show_current ) echo $sep;
+			if ( $show_current ) echo $before . get_the_time('Y') . $after;
+			elseif ( $show_home_link && $show_last_sep ) echo $sep;
+
+		} elseif ( is_month() ) {
+			if ( $show_home_link ) echo $sep;
+			$position += 1;
+			echo sprintf( $link, get_year_link( get_the_time('Y') ), get_the_time('Y'), $position );
+			if ( $show_current ) echo $sep . $before . get_the_time('F') . $after;
+			elseif ( $show_last_sep ) echo $sep;
+
+		} elseif ( is_day() ) {
+			if ( $show_home_link ) echo $sep;
+			$position += 1;
+			echo sprintf( $link, get_year_link( get_the_time('Y') ), get_the_time('Y'), $position ) . $sep;
+			$position += 1;
+			echo sprintf( $link, get_month_link( get_the_time('Y'), get_the_time('m') ), get_the_time('F'), $position );
+			if ( $show_current ) echo $sep . $before . get_the_time('d') . $after;
+			elseif ( $show_last_sep ) echo $sep;
+
+		} elseif ( is_single() && ! is_attachment() ) {
+			if ( get_post_type() != 'post' ) {
+				$position += 1;
+				$post_type = get_post_type_object( get_post_type() );
+				if ( $position > 1 ) echo $sep;
+				echo sprintf( $link, get_post_type_archive_link( $post_type->name ), $post_type->labels->name, $position );
+				if ( $show_current ) echo $sep . $before . get_the_title() . $after;
+				elseif ( $show_last_sep ) echo $sep;
+			} else {
+				$cat = get_the_category(); $catID = $cat[0]->cat_ID;
+				$parents = get_ancestors( $catID, 'category' );
+				$parents = array_reverse( $parents );
+				$parents[] = $catID;
+				foreach ( $parents as $cat ) {
+					$position += 1;
+					if ( $position > 1 ) echo $sep;
+					echo sprintf( $link, get_category_link( $cat ), get_cat_name( $cat ), $position );
+				}
+				if ( get_query_var( 'cpage' ) ) {
+					$position += 1;
+					echo $sep . sprintf( $link, get_permalink(), get_the_title(), $position );
+					echo $sep . $before . sprintf( $text['cpage'], get_query_var( 'cpage' ) ) . $after;
+				} else {
+					if ( $show_current ) echo $sep . $before . get_the_title() . $after;
+					elseif ( $show_last_sep ) echo $sep;
+				}
+			}
+
+		} elseif ( is_post_type_archive() ) {
+			$post_type = get_post_type_object( get_post_type() );
+			if ( get_query_var( 'paged' ) ) {
+				$position += 1;
+				if ( $position > 1 ) echo $sep;
+				echo sprintf( $link, get_post_type_archive_link( $post_type->name ), $post_type->label, $position );
+				echo $sep . $before . sprintf( $text['page'], get_query_var( 'paged' ) ) . $after;
+			} else {
+				if ( $show_home_link && $show_current ) echo $sep;
+				if ( $show_current ) echo $before . $post_type->label . $after;
+				elseif ( $show_home_link && $show_last_sep ) echo $sep;
+			}
+
+		} elseif ( is_attachment() ) {
+			$parent = get_post( $parent_id );
+			$cat = get_the_category( $parent->ID ); $catID = $cat[0]->cat_ID;
+			$parents = get_ancestors( $catID, 'category' );
+			$parents = array_reverse( $parents );
+			$parents[] = $catID;
+			foreach ( $parents as $cat ) {
+				$position += 1;
+				if ( $position > 1 ) echo $sep;
+				echo sprintf( $link, get_category_link( $cat ), get_cat_name( $cat ), $position );
+			}
+			$position += 1;
+			echo $sep . sprintf( $link, get_permalink( $parent ), $parent->post_title, $position );
+			if ( $show_current ) echo $sep . $before . get_the_title() . $after;
+			elseif ( $show_last_sep ) echo $sep;
+
+		} elseif ( is_page() && ! $parent_id ) {
+			if ( $show_home_link && $show_current ) echo $sep;
+			if ( $show_current ) echo $before . get_the_title() . $after;
+			elseif ( $show_home_link && $show_last_sep ) echo $sep;
+
+		} elseif ( is_page() && $parent_id ) {
+			$parents = get_post_ancestors( get_the_ID() );
+			foreach ( array_reverse( $parents ) as $pageID ) {
+				$position += 1;
+				if ( $position > 1 ) echo $sep;
+				echo sprintf( $link, get_page_link( $pageID ), get_the_title( $pageID ), $position );
+			}
+			if ( $show_current ) echo $sep . $before . get_the_title() . $after;
+			elseif ( $show_last_sep ) echo $sep;
+
+		} elseif ( is_tag() ) {
+			if ( get_query_var( 'paged' ) ) {
+				$position += 1;
+				$tagID = get_query_var( 'tag_id' );
+				echo $sep . sprintf( $link, get_tag_link( $tagID ), single_tag_title( '', false ), $position );
+				echo $sep . $before . sprintf( $text['page'], get_query_var( 'paged' ) ) . $after;
+			} else {
+				if ( $show_home_link && $show_current ) echo $sep;
+				if ( $show_current ) echo $before . sprintf( $text['tag'], single_tag_title( '', false ) ) . $after;
+				elseif ( $show_home_link && $show_last_sep ) echo $sep;
+			}
+
+		} elseif ( is_author() ) {
+			$author = get_userdata( get_query_var( 'author' ) );
+			if ( get_query_var( 'paged' ) ) {
+				$position += 1;
+				echo $sep . sprintf( $link, get_author_posts_url( $author->ID ), sprintf( $text['author'], $author->display_name ), $position );
+				echo $sep . $before . sprintf( $text['page'], get_query_var( 'paged' ) ) . $after;
+			} else {
+				if ( $show_home_link && $show_current ) echo $sep;
+				if ( $show_current ) echo $before . sprintf( $text['author'], $author->display_name ) . $after;
+				elseif ( $show_home_link && $show_last_sep ) echo $sep;
+			}
+
+		} elseif ( is_404() ) {
+			if ( $show_home_link && $show_current ) echo $sep;
+			if ( $show_current ) echo $before . $text['404'] . $after;
+			elseif ( $show_last_sep ) echo $sep;
+
+		} elseif ( has_post_format() && ! is_singular() ) {
+			if ( $show_home_link && $show_current ) echo $sep;
+			echo get_post_format_string( get_post_format() );
+		}
+
+		echo $wrap_after;
+
+	}
+} // end of the_breadcrumbs()
